465d4806f81fd6541baaaa5b5b98d13783c099ae
LisaYli <lisa.ylikangas@hotmail.com>
2022-01-14 15:54:00 +0100

    Adds the CellGeneration class, that contains the readFirstGeneration(), it reads the first generation from a file and returns a String. No Test for this:)

---
+++ src/main/java/gameoflife/CellGeneration.java
@@ -0,0 +1,20 @@
+package gameoflife;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CellGeneration {
+
+    public static String readFirstGeneration() throws FileNotFoundException {
+        File file = new File("src/main/resources/first-generation");
+        Scanner sc = new Scanner(file);
+        // we just need to use \\Z as delimiter
+        return sc.useDelimiter("\\Z").next();
+    }
+
+    public void getToNextGeneration(Cell cell) {
+        //TODO: skapa levande cell then(testa använda cell.getNextCellState)
+    }
+
+}
