2315cbbe8f70f4ec02c127df80b6755b95b0c0dc
lisyli2 <lisa.ylikangas@hotmail.com>
2024-09-11 15:53:40 +0200

    WIP | Reading from board.txt with stream

---
+++ src/main/java/gameoflife/Board.java
@@ -0,0 +1,47 @@
+package gameoflife;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+utdata kan se ut såhär:
+Generation 2:
+4 8
+........
+...**...
+...**...
+........
+ */
+public class Board {
+
+    private List<List<Cell>> board;
+
+    public Board(String filePath) throws IOException {
+        board = Files.lines(Paths.get(filePath))
+                .map(this::parseLine)
+                .collect(Collectors.toList());
+    }
+
+    private List<Cell> parseLine(String line) {
+        return line.chars()
+                .mapToObj(c -> c == '*' ? new Cell(LifeStatus.ALIVE) : new Cell(LifeStatus.DEAD))
+                .collect(Collectors.toList());
+    }
+
+    public void printBoard() {
+        board.forEach(row -> {
+            row.forEach(cell -> System.out.println(cell.toString()));
+            System.out.println();
+        });
+    }
+
+    public static void main(String[] args) throws IOException {
+        Board board1 = new Board("src/main/resources/board.txt");
+        board1.printBoard();    }
+
+
+
+}
