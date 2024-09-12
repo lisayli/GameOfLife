3d47798e502e5c061d534ef916a92ba8a71a3794
lisyli2 <lisa.ylikangas@hotmail.com>
2024-09-12 14:24:38 +0200

    printBard() - method implementation works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Board board = new Board("src/main/resources/board.txt");
+        Board board = new Board("src/main/resources/board1.txt");
 
         board.printBoard();
 
