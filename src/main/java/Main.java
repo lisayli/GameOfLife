508518753ca6d9c8d29fa130f966d9f40a46a740
lisyli2 <lisa.ylikangas@hotmail.com>
2024-09-16 14:54:49 +0200

    displaying up to generation 2 in main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,22 @@
 import gameoflife.Board;
+import gameoflife.Cell;
 
 import java.io.IOException;
-import java.nio.file.Paths;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         Board board = new Board("src/main/resources/board1.txt");
 
+        System.out.println("Generation 1:");
+        board.printBoard();
+
+        board.getNextGeneration();
+
+        System.out.println("Generation 2:");
         board.printBoard();
 
     }
-
 
 }
